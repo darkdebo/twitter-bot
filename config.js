96802b44c13dc58047bd548005badbfad161611a
Debojyoti Chakraborty <50295688+darkdebo@users.noreply.github.com>
2020-03-15 09:11:30 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: '7pCncjjmdjTHdyUHQKouXfzqR',  
+    consumer_secret: 'hk8xHzrIrrcJfAZoef9XjqavWOQLnLuJsKaQdzjdNdREbbRkO0',
+    access_token: '876635674883084289-EyXSSAUP3cGtIKQGBw8IGHePSzNPyRd',  
+    access_token_secret: 'u834K3M1fLMlZr9utqLTH4fChgsF7Ez0nDzpLqs7UtSXf'
+  }
